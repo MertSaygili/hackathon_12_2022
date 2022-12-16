9c0e3f2bf28ccae44dc9ce7773576b19cd095e62
enesuzun2002 <emurat.uzun@gmail.com>
2022-12-16 20:26:25 +0300

    firestore services

---
+++ hackathon_app/lib/core/base/services/db/ref.dart
@@ -0,0 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class RefService {
+  static final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  static CollectionReference get users => _db.collection("users");
+  static CollectionReference get listings => _db.collection("listings");
+  static CollectionReference get comments => _db.collection("comments");
+}
