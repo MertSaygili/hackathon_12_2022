c842844bb27ad5a53a765e46a84a07faa72f6108
saltukozelgul <saltukozelgul@gmail.com>
2022-12-17 01:39:56 +0300

    categories done

--- hackathon_app/lib/view/home/home_page.dart
+++ hackathon_app/lib/view/home/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hackathon_app/core/components/appbar/custom_appbar.dart';
+import 'package:hackathon_app/core/components/icon_buttons/circle_icon_button.dart';
 import 'package:hackathon_app/core/components/textfield/custom_textfield.dart';
+import 'package:hackathon_app/core/components/divider/custom_line_divider.dart';
+import 'package:hackathon_app/core/constants/app/colors.dart';
 
 import '../../core/constants/app/strings.dart';
 
@@ -13,6 +16,24 @@ class HomePageView extends StatefulWidget {
 }
 
 class _HomePageViewState extends State<HomePageView> {
+  // Create a list that holds icons
+  final List<IconData> _icons = <IconData>[
+    Icons.electric_bolt,
+    Icons.bed_outlined,
+    Icons.baby_changing_station,
+    Icons.sports_basketball_outlined,
+    Icons.person_outline,
+  ];
+
+  // Create a list that holds categories
+  final List<String> _categoriesText = <String>[
+    'Electronics',
+    'Furniture',
+    'Baby',
+    'Sports',
+    'Fashion',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -35,8 +56,70 @@ class _HomePageViewState extends State<HomePageView> {
           preferedSize: 75,
         ),
         body: Column(
-          children: [],
+          children: [
+            _categories(),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget _categories() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const CustomLineDivider(
+            lineColor: colorPrimary,
+          ),
+          Text(
+            "Categories",
+            style: Theme.of(context).textTheme.headline6!.copyWith(
+                height: 1,
+                color: colorBlack,
+                fontSize: 25,
+                fontWeight: FontWeight.w800,
+                fontFamily: 'BalooBhaina',
+                shadows: <Shadow>[
+                  const Shadow(
+                    blurRadius: 4.0,
+                    color: Color.fromARGB(150, 0, 0, 0),
+                  )
+                ]),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 100,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 5,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Column(
+                      children: [
+                        CircleIconButton(
+                          backgroundColor: colorPrimary,
+                          color: colorWhite,
+                          radius: 30,
+                          iconSize: 35,
+                          icon: _icons[index],
+                          pressFunction: () {},
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 3.0),
+                          child: Text(_categoriesText[index],
+                              style:
+                                  const TextStyle(fontStyle: FontStyle.italic)),
+                        )
+                      ],
+                    ),
+                  );
+                }),
+          ),
+        ],
       ),
     );
   }
