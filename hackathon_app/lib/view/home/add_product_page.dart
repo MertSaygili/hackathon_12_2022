df9ebb9d2604c8b7d82e998724737a24757ed885
MertSaygili <mert71719601@gmail.com>
2022-12-17 03:50:55 +0300

    s

--- hackathon_app/lib/view/home/add_product_page.dart
+++ hackathon_app/lib/view/home/add_product_page.dart
@@ -1,12 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hackathon_app/core/components/appbar/custom_appbar.dart';
-import 'package:hackathon_app/core/components/bottom_navigation_bar/page_view.dart';
-import 'package:hackathon_app/view/home/add_product_pages/favourites.dart';
-import 'package:hackathon_app/view/home/add_product_pages/mylistings.dart';
-
-import '../../core/components/bottom_navigation_bar/bottom_navigation_bar.dart';
-import '../../core/constants/app/colors.dart';
-import '../../core/constants/app/strings.dart';
+import 'package:hackathon_app/core/constants/app/colors.dart';
+import 'package:hackathon_app/core/constants/app/strings.dart';
 
 class AddProductPage extends StatefulWidget {
   const AddProductPage({super.key});
@@ -16,11 +11,21 @@ class AddProductPage extends StatefulWidget {
 }
 
 class _AddProductPageState extends State<AddProductPage> {
-  late final PageController _controller = PageController();
-  final List<Widget> _items = [];
-
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: CustomAppBar(
+        backgroundColor: colorScaffold,
+        preferedSize: 55,
+        backButton: true,
+        actions: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.asset(logoUrlSmall, height: 50),
+        ),
+      ),
+      body: Column(
+        children: const [],
+      ),
+    );
   }
 }
